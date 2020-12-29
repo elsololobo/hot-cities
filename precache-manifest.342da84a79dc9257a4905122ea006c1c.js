4dec3ba8319fe68b28482e09da317f9eb7bbe944
Goga Papidze <goga.papidze@lb.com>
2020-12-29 17:57:13 +0400

    Updates

---
+++ precache-manifest.342da84a79dc9257a4905122ea006c1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cee229640135d613544c40c79eedd4",
+    "url": "/hot-cities/index.html"
+  },
+  {
+    "revision": "2ea010b69a0bd5515bc3",
+    "url": "/hot-cities/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "730397c70a036df8b19d",
+    "url": "/hot-cities/static/css/main.e8bbc88b.chunk.css"
+  },
+  {
+    "revision": "2ea010b69a0bd5515bc3",
+    "url": "/hot-cities/static/js/2.2f0bbaf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hot-cities/static/js/2.2f0bbaf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730397c70a036df8b19d",
+    "url": "/hot-cities/static/js/main.8cbe5449.chunk.js"
+  },
+  {
+    "revision": "ced8d6e063aab042061a",
+    "url": "/hot-cities/static/js/runtime-main.1fd2f61a.js"
+  },
+  {
+    "revision": "5e600e115f0cde658e00d72a7ea467a1",
+    "url": "/hot-cities/static/media/correct.5e600e11.svg"
+  },
+  {
+    "revision": "e98fe79745768a02b0f52ca77eddd21b",
+    "url": "/hot-cities/static/media/wrong.e98fe797.svg"
+  }
+]);
